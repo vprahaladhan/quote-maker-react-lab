02e9a7eb174f4b80629811fa61a34210db33b775
prahal <vprahaladhan@gmail.com>
2021-06-26 20:46:08 +1000

    commit tree 01ac4bda04a835e8cf652e0f471f6c1ce3f2c5f5

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -7,10 +7,16 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
 
   handleOnChange = event => {
     // Handle Updating Component State
+    this.setState(prevState => ({
+      content: event.target.name === 'content' ? event.target.value : prevState.content,
+      author: event.target.name === 'author' ? event.target.value : prevState.author
+    }))
   }
 
   handleOnSubmit = event => {
@@ -18,6 +24,16 @@ class QuoteForm extends Component {
     // Create quote object from state
     // Pass quote object to action creator
     // Update component state to return to default state
+    event.preventDefault()
+    this.props.addQuote({
+      content: this.state.content,
+      author: this.state.author,
+      votes: 0
+    })
+    this.setState({
+      content: '',
+      author: ''  
+    })
   }
 
   render() {
@@ -27,13 +43,15 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
+                        name="content"
                         className="form-control"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -41,9 +59,11 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -63,4 +83,8 @@ class QuoteForm extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+const mapDispatchToProps = dispatch => ({
+  addQuote: quote => dispatch(addQuote(quote))
+})
+
+export default connect(() => {}, mapDispatchToProps)(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
 export default (state = [], action) => {
-  return state;
-}
+  switch(action.type) {
+    case 'ADD_QUOTE': 
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE': 
+      const index = state.indexOf(state.find(quote => quote.id === action.quoteId))
+      let new_state = [...state]
+      new_state.splice(index, 1)
+      return new_state
+
+    case 'UPVOTE_QUOTE':
+      const upQuote = state.find(quote => quote.id === action.quoteId)
+      const upIndex = state.indexOf(upQuote)
+      let up_new_state = [...state]
+      console.log("Quote before adding vote > ", state.find(quote => quote.id === action.quoteId))
+      up_new_state.splice(upIndex, 1, {...upQuote, votes: upQuote.votes + 1})
+      console.log("Quote after adding vote > ", up_new_state.find(quote => quote.id === action.quoteId))
+      return up_new_state
+
+    case 'DOWNVOTE_QUOTE':
+      const downQuote = state.find(quote => quote.id === action.quoteId)
+      const downIndex = state.indexOf(downQuote)
+      let down_new_state = [...state]
+      down_new_state.splice(downIndex, 1, {...downQuote, votes: downQuote.votes > 0 ? downQuote.votes - 1 : 0})
+      return down_new_state
+
+    default: return state;
+  }
+};
